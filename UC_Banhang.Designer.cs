39a64e56f8dec6a2e2428fe89ff8d2154d4c1a2e
nbich2302 <ngobich2302@gmail.com>
2025-05-27 16:53:07 +0700

    banhang

---
+++ UC_Banhang.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace BookStore
+{
+    partial class UC_Banhang
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UC_Banhang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UC_Banhang";
+            this.Size = new System.Drawing.Size(761, 451);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ UC_Banhang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore
+{
+    public partial class UC_Banhang : UserControl
+    {
+        public UC_Banhang()
+        {
+            InitializeComponent();
+        }
+    }
+}
