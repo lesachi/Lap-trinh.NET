917c7e15f615622f02c5209711a5e750110d040f
minhhang1306 <hangduong1112004@gmail.com>
2025-05-30 03:16:57 +0700

    fix NXB

--- UC_ChitietNXB.cs
+++ UC_ChitietNXB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,11 @@ namespace BookStore
         }
 
         private void UC_ChitietNXB_Load(object sender, EventArgs e)
+        {
+            Database.GetConnection();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
         {
 
         }
